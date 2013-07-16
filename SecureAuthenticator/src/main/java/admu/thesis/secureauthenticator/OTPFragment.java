873f3720909bbd5416b20d861a5451507a8517d5
Mariano Luis Uymatiao <wizfactormail@gmail.com>
2013-07-16 20:02:46 +0800

    Implement cyclical 30-second clocks in OTPFragment.java

--- SecureAuthenticator/src/main/java/admu/thesis/secureauthenticator/OTPFragment.java
+++ SecureAuthenticator/src/main/java/admu/thesis/secureauthenticator/OTPFragment.java
@@ -5,11 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Ian on 7/16/13.
  */
-public class OTPFragment extends Fragment {
+public class OTPFragment extends Fragment implements Runnable{
+    //Base Time determines the counter value during TOTP generation
+    double baseTime; //Base time counter remains unchanged for 30 seconds
+    double newBaseTime;
+    double newTime; //Always keeps track of current time;
+
+    double percent;
+    int progress; //Progress until next 30 seconds
+
+    ProgressBar timeProgress;
+    TextView timeCounter; //Displays base time counter
+
     public OTPFragment() {
     }
 
@@ -18,6 +31,52 @@ public class OTPFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_otp, container, false);
 
+        //Establish a thread for tracking current time
+        new Thread(this).start();
+
         return rootView;
+    }
+
+    //Thread code
+    public void run() {
+        try {
+            baseTime = Math.floor(System.currentTimeMillis() / 30000);
+
+            System.out.println(baseTime);
+            while(true) {
+                newTime = System.currentTimeMillis();
+
+                //System.out.println(newTime);
+
+                newBaseTime = Math.floor(newTime / 30000);
+
+                if(newBaseTime > baseTime) {
+                    baseTime = newBaseTime;
+
+                    System.out.println(baseTime);
+
+                    getActivity().runOnUiThread(new Runnable() {
+
+                        public void run() {
+                            timeCounter = (TextView) getActivity().findViewById(R.id.timeCounter);
+                            timeCounter.setText(Double.valueOf(baseTime).toString());
+                        }
+                    });
+                }
+
+                progress = (int) ((newTime % 30000.0) / 30000 * 300);
+
+                System.out.println(progress);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        timeProgress = (ProgressBar) getActivity().findViewById(R.id.timeProgress);
+                        timeProgress.setProgress(progress);
+                    }
+                });
+
+                Thread.sleep(500);
+            }
+        } catch(Exception e) {}
     }
 }
